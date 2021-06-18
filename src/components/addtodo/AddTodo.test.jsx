2afee7bb4c86d6538a6afcde89a65531987a9b5b
kemppi83 <kemppi83@gmail.com>
2021-06-18 16:58:07 +0200

    added react testing library linter

--- src/components/addtodo/AddTodo.test.jsx
+++ src/components/addtodo/AddTodo.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import AddTodo from './AddTodo';
 
 test('renders learn react link', () => {
   const setTodos = a => { console.log(a); };
-  const { getByPlaceholderText, getByText } = render(<AddTodo setTodos={setTodos} />);
-  getByPlaceholderText('Title*');
-  getByPlaceholderText('Description');
-  getByText('Add');
+  render(<AddTodo setTodos={setTodos} />);
+  screen.getByPlaceholderText('Title*');
+  screen.getByPlaceholderText('Description');
+  screen.getByText('Add');
 });
--- src/components/todocard/TodoCard.test.jsx
+++ src/components/todocard/TodoCard.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TodoCard from './TodoCard';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<TodoCard
+  render(<TodoCard
     todo={{
       description: 'This is description',
       id: '42bdf00f-11a3-402f-b0ff-7a7057ed5da8',
@@ -14,6 +14,6 @@ test('renders learn react link', () => {
     changeStatus={e => console.log(e.target)}
     removeTodo={e => console.log(e.target)} />);
 
-  getByText('This is description');
-  getByText('This is title');
+  screen.getByText('This is description');
+  screen.getByText('This is title');
 });
